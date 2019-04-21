bd936c3085da859c03dd4150931052bff553892b
HideakiTouhara <hideee6723@gmail.com>
2019-04-21 20:12:11 +0900

    bubble sort

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -9,29 +9,13 @@ int bubbleSort(int A[], int N) {
         for(int j = N - 1; j >= i + 1; j--) {
             if(A[j] < A[j - 1]) {
                 swap(A[j], A[j - 1]);
-                flag = 1;
                 sw++;
+                flag = 1;
             }
         }
     }
     return sw;
 }
-
-// int bubbleSort(int A[], int N) {
-//     int sw = 0;
-//     bool flag = 1;
-//     for(int i = 0; flag; i++) {
-//         flag = 0;
-//         for(int j = N - 1; j >= i + 1; j--) {
-//             if(A[j] > A[j - 1]) {
-//                 swap(A[j], A[j - 1]);
-//                 sw++;
-//                 flag = 1;
-//             }
-//         }
-//     }
-//     return sw;
-// }
 
 
 int main() {
