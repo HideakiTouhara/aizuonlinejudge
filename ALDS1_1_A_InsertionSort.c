c87359dcd1b61edd94bfa8912f4bb2a6534d1209
HideakiTouhara <hideee6723@gmail.com>
2019-04-21 19:34:44 +0900

    update

--- ALDS1_1_A_InsertionSort.c
+++ ALDS1_1_A_InsertionSort.c
@@ -9,11 +9,11 @@ void trace(int A[], int N) {
 }
 
 void insertionSort(int A[], int N) {
-    int j, i, v;
-    for(i = 1; i < N; i++) {
+    int v, j;
+    for(int i = 1; i < N; i++) {
         v = A[i];
         j = i - 1;
-        while(j >= 0 && A[j] > v) {
+        while(j >= 0 && v < A[j]) {
             A[j + 1] = A[j];
             j--;
         }
@@ -23,14 +23,11 @@ void insertionSort(int A[], int N) {
 }
 
 int main() {
-    int N, i, j;
+    int N;
     int A[100];
-
     scanf("%d", &N);
-    for(i = 0; i < N; i++) scanf("%d", &A[i]);
-
+    for(int i = 0; i < N; i++) { scanf("%d", &A[i]); }
     trace(A, N);
     insertionSort(A, N);
-
     return 0;
 }
